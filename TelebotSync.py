63140f5eb3e19e90ea17e345c267eb880b7a85a7
darrenlsx <darren.lim@live.com.sg>
2023-07-03 16:44:02 +0800

    Fix bug and change add and start functionality

--- TelebotSync.py
+++ TelebotSync.py
@@ -43,25 +43,21 @@ if not creds or not creds.valid:
 service = build('calendar', 'v3', credentials=creds)
 db = Firebase()
 
-@bot.message_handler(commands=['start'])
-def welcome_message(message):
-    bot.send_message(message.chat.id, "Hello, welcome to N.Trance Sync, enter /menu to begin :>")
-    db.get_or_create_user(message.chat.id)
-
 # List of Commands
 @bot.message_handler(commands=['add'])
 def create_event(message):
-    reply = bot.send_message(message.chat.id, "What is the song title to add?")
+    reply = bot.send_message(message.chat.id, "What is the song title to add? Note no symbols pls only letters and whitespaces")
     def get_song_name(message):
         global song_name
         song_name = message.text
-        
-        db.get_or_create_song(song_name)        
-        
+        print(song_name)
         bot.send_message(message.chat.id, "Okai adding for song title: {}\n\nPlease type in the dates in the proper format\nDate: DD/MM _(day)_\nTime: HH.MMam/pm - HH.MMam/pm\nCMI:\nAgenda:\n\nYou may leave CMI or Agenda empty.\nThe (day) is optional, doesnt affect the bot, just for your ez ref if want to paste in grpchat.\nIf you need an example enter /eg and go back to /add again to continue.\nYou can copy the below message to get started.".format(song_name), parse_mode="Markdown")
         bot.send_message(message.chat.id, "Date:\nTime:\nCMI:\nAgenda:\n ")
-        
     bot.register_next_step_handler(reply, get_song_name)
+
+@bot.message_handler(commands=['start'])
+def welcome_message(message):
+    bot.send_message(message.chat.id, "Hello, welcome to N.Trance Sync, enter /menu to begin :>")
 
 @bot.message_handler(commands=['menu'])
 def handle_m(message):
@@ -243,7 +239,7 @@ def parse_time(time):
     if (len(hr) < 2):
         hr = "0" + hr
 
-    if (am_pm == "pm"):
+    if (am_pm == "pm" and int(hr) != 12):
         hr = str(int(hr) + 12)
 
     return [hr, mins]
